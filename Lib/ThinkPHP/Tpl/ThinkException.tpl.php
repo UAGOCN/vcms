e1b5af38966785175140ad7cb8eddc776fc6531c
UAGOCN <61579539+UAGOCN@users.noreply.github.com>
2020-11-22 02:03:04 +0800

    vcms
    vcms

--- Lib/ThinkPHP/Tpl/ThinkException.tpl.php
+++ Lib/ThinkPHP/Tpl/ThinkException.tpl.php
@@ -2,10 +2,9 @@
 <!--STATUS OK-->
 <html lang="zh-cmn-Hans">
 <head>
-<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <meta name="Generator" content="EditPlus"/>
 <title>系统发生错误</title>
 <style>
