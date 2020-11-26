86344868552b90938679d3718bdee4031bf9cd69
UAGOCN <61579539+UAGOCN@users.noreply.github.com>
2020-11-26 15:52:36 +0800

    vcms
    vcms

--- admin.php
+++
@@ -1,9 +0,0 @@
-<?php
-register_shutdown_function('session_write_close');
-session_set_cookie_params(0, '/; samesite=lax', '.san.tv', TRUE, TRUE);
-session_name('__cflb');
-session_start();
-$_SESSION['AdminLogin'] = 1;
-$_SESSION['host_key'] = ''; // 后台登陆域名,只允许唯一该域名访问后台管理界面
-header("Location: ./index.php?s=Admin-Login"); 
-?>
