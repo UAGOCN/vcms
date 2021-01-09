7774e21933b6e9697d0b7a7b6a44c8dd3676dd78
UAGOCN <61579539+UAGOCN@users.noreply.github.com>
2021-01-09 23:44:52 +0800

    vcms
    vcms

---
+++ upurl.php
@@ -0,0 +1,28 @@
+<?php 
+header("Content-type:text/html;charset=utf-8");
+date_default_timezone_set("PRC");
+
+$id = !empty($_GET['id'])?intval($_GET['id']):1;
+$end = !empty($_GET['end'])?intval($_GET['end']):1;
+
+if(!empty($id)&&$id<=$end){
+    $SqlID = 'UPDATE ff_vod SET vod_url=\'https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8?'.time().'\', vod_continu=\'HD1280\' WHERE vod_id='.$id;
+    getInsert('127.0.0.1','root','root','root',$SqlID);
+    echo '正在 第'.$id.'条 更新<script>setTimeout(function(){window.location.href="/upurl.php?id='.($id+1).'&end='.$end.'";}, 1000);</script>';
+} else {
+    echo '更新完毕';
+}
+
+function getInsert($host, $user, $passwd, $db, $sql){
+    try {
+        $conn = new PDO('mysql:host='.$host.';port=3306;dbname='.$db, $user, $passwd);
+        $conn->query("set names utf8");
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conn->exec($sql);
+        $conn = null;
+        return "新记录插入成功";
+    } catch(PDOException $e) {
+        $conn = null;
+        return $sql . "<br>" . $e->getMessage();exit();
+    }
+}
