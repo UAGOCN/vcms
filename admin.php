3cc5e95931b80703e052ada7abef03e74abb37f0
UAGOCN <61579539+UAGOCN@users.noreply.github.com>
2021-01-09 23:46:57 +0800

    vcms
    vcms

--- admin.php
+++
@@ -1,9 +0,0 @@
-<?php
-register_shutdown_function('session_write_close');
-session_set_cookie_params(['lifetime' => time()+900,'path' => '/','domain' => $_SERVER['HTTP_HOST'],'secure' => TRUE,'httponly' => TRUE,'samesite' => 'lax']);
-session_name('__cflb');
-session_start();
-$_SESSION['AdminLogin'] = 1;
-$_SESSION['host_key'] = ''; // 后台登陆域名,只允许唯一该域名访问后台管理界面
-header("Location: ./index.php?s=Admin-Login"); 
-?>
